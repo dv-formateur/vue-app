4fc55c7bb859b90778ed57ed6152221d823072a9
ompuno <ompuno@yahoo.fr>
2020-06-10 15:00:39 +0200

    Test vue-app

---
+++ src/views/Daniel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+   
+    Je suis dans le composant Daniel.vue
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Daniel",
+  components: {
+    
+  }
+};
+</script>
+
+<style>
+/*.home {
+	background-color: red;
+}*/
+</style>
